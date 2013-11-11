c013ffae87b848dd73bb8c7849e3da29712013b1
mambrose88 <m.ambrose88@gmail.com>
2013-11-11 09:05:39 -0500

    updated

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
 // JavaScript Document
 onload=init;
-
+//create an array to keep track of items currently in play
+var coin_array = new Array();
 function init(){
+	for (var i=0; i<20; i++){
 	//make an object paste on the coin class:
-	var coin1 = new Coin();
-	coin1.create();
-	var coin2 = new Coin();
-	coin2.create();
-	var coin3 = new Coin();
-	coin3.create();
-	var coin4 = new Coin();
-	coin4.create();
-	var coin5 = new Coin();
-	coin5.create();
-	var coin6 = new Coin();
-	coin6.create();
-
-}
+	var anothercoin = new Coin();
+	anothercoin.create();
+	//put that new coin in array
+	coin_array.push(anothercoin);
+	//when the new coin is clicked, use its built-in destroy function, passing the event
+	anothercoin.item_on_page.onclick = function(e){
+		anothercoin.destroy(e);
+	   }//end event handler
+	}//close for loop
+}//close init
 
 //lets make a class (blueprint) for each coin we generate
 function Coin(){
@@ -30,8 +28,8 @@ function Coin(){
 		//make a section element in the HTML, store it into the item on page we set up above
 	this.item_on_page = document.createElement("section");
 		//store a random x and y position different for each coin. I'm using screen width of 500, height of 300:
-	this.x = Math.floor(Math.random()*500);
-	this.y = Math.floor(Math.random()*300);
+	this.x = Math.floor(Math.random()*700);
+	this.y = Math.floor(Math.random()*500);
 	
 		//use those x and y coordinates in the css to position the coins:
 	this.item_on_page.style.left = this.x + "px";
@@ -46,7 +44,14 @@ function Coin(){
 	/**function does lots of things when a coin gets removed on the page
 	 *
 	*/
-	this.destroy = function(){
+	this.destroy = function(e){
+//		console.log("destroyed");
+	document.getElementsByTagName("body")[0].removeChild(e.target);
+	//figure out that coins position in the array
+	var this_coins_index_number = coin_array.indexOf(this);
+	//splice it out of the array
+	coin_array.splice(this_coins_index_number,1);
+	console.log(coin_array.length);
 	}
 
 
